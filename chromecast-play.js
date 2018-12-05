48f10d87340a1e14a0c7efd1f747d29eb71473c0
jonferreira <my.mail.jonatas@gmail.com>
2018-12-05 18:42:32 +0000

    Update chromecast-play.js

--- chromecast-play.js
+++ chromecast-play.js
@@ -3,76 +3,105 @@ var DefaultMediaReceiver  = require('castv2-client').DefaultMediaReceiver;
 
 function play(host, url, type, volume, node, msg) {
 
-  var client = new Client();
-
-  client.connect(host, function() {
   
-	if(volume) {
-		console.log('connected, launching app on %s with url %s, type %s and volume %s', host, url, type, volume);
-	} else { 
-		console.log('connected, launching app on %s with url %s and type %s', host, url, type);
-	}
-	
-    client.launch(DefaultMediaReceiver, function(err, player) {
-      var media = {
+  
+  try {
 
-        // Here you can plug an URL to any mp4, webm, mp3 or jpg file with the proper contentType.
-        contentId: url,
-        contentType: type,
-        streamType: 'BUFFERED' // or LIVE    
-      };
+	  var client = new Client();
+
+	  client.connect(host, function() {
 	  
-	  if(typeof volume !== 'undefined') {
-	  
-		if(volume > 0){
-			var obj = { level: volume/100 };
-		} else {
-			var obj = { muted: true };
+		if(volume) {
+			console.log('connected, launching app on %s with url %s, type %s and volume %s', host, url, type, volume);
+		} else { 
+			console.log('connected, launching app on %s with url %s and type %s', host, url, type);
 		}
 		
-		  client.setVolume(obj, function(err, newvol){
-				if(err) {
-					console.log("there was an error setting the volume")
-					node.error("Chromecast error: there was an error setting the volume");
-					msg.error = "there was an error setting the volume";
-					node.send(msg);
-					return node.status({
-					  shape: "dot",
-					  fill: "red",
-					  text: "error: there was an error setting the volume" 
-					});
-				}
-				console.log("volume changed to %s", Math.round(newvol.level * 100))
-			});
-			
-	  }
-		
-      player.load(media, { autoplay: true }, function(err, status) {
-		if(status) {
-			console.log('media loaded playerState=%s', status.playerState);
-			node.status({});
-		}
-		if(err) {
-			console.log('media loaded error= %s', err);
-			msg.error = " " +err;
-			node.send(msg);
-			return node.status({
-			  shape: "dot",
-			  fill: "red",
-			  text: " " +err
-			});
-			node.error("Chromecast error: " + err);
-		}
-		
-          client.close();
+		client.launch(DefaultMediaReceiver, function(err, player) {
+		  var media = {
+
+			// Here you can plug an URL to any mp4, webm, mp3 or jpg file with the proper contentType.
+			contentId: url,
+			contentType: type,
+			streamType: 'BUFFERED' // or LIVE    
+		  };
 		  
-      });
-	  
-	  node.send(msg);
-	  
-    });
+		  if(typeof volume !== 'undefined') {
+		  
+			if(volume > 0){
+				var obj = { level: volume/100 };
+			} else {
+				var obj = { muted: true };
+			}
+			
+			  client.setVolume(obj, function(err, newvol){
+					if(err) {
+						console.log("there was an error setting the volume")
+						node.error("Chromecast error: there was an error setting the volume");
+						msg.error = "there was an error setting the volume";
+						node.send(msg);
+						return node.status({
+						  shape: "dot",
+						  fill: "red",
+						  text: "error: there was an error setting the volume" 
+						});
+					}
+					console.log("volume changed to %s", Math.round(newvol.level * 100))
+				});
+				
+		  }
+			
+		  player.load(media, { autoplay: true }, function(err, status) {
+			if(status) {
+				console.log('media loaded playerState=%s', status.playerState);
+				node.status({});
+			}
+			if(err) {
+				console.log('media loaded error= %s', err);
+				msg.error = " " +err;
+				node.send(msg);
+				node.error("Chromecast error: " + err);
+				return node.status({
+				  shape: "dot",
+				  fill: "red",
+				  text: " " +err
+				});
+				
+			}
+			
+			  client.close();
+			  
+		  });
+		  
+		  node.send(msg);
+		  
+		});
 
-  });
+	  });
+	  
+	  client.on('error', function(err) {
+		msg.error = " " +err;
+		node.send(msg);
+		node.error("Chromecast " + err);
+		return node.status({
+		  shape: "dot",
+		  fill: "red",
+		  text: " " +err
+		});
+		
+		client.close();
+	  });
+  
+  }
+  catch (err) {
+		node.status({
+		  shape: "dot",
+		  fill: "red",
+		  text: " " +err
+		});
+		node.error("Chromecast error: " + err);
+	}
+  
 }
 
 module.exports = function(RED) {
@@ -97,7 +126,17 @@ module.exports = function(RED) {
             }
 
             if (payload.ip && payload.url && payload.contentType) {
-                play(payload.ip, payload.url, payload.contentType, payload.volume, node, msg);
+				try {
+					play(payload.ip, payload.url, payload.contentType, payload.volume, node, msg);
+				}
+				catch (err) {
+					node.status({
+					  shape: "dot",
+					  fill: "red",
+					  text: " " +err
+					});
+					node.error("Chromecast error: " + err);
+				}
             }
 			
 			//node.send(msg);
