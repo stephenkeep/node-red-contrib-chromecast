09ec8491385b4d63ceac62b092ca67cc0ee360a5
Jan Kretschmann <jan.kretschmann@sitec-technology.de>
2019-03-08 12:50:27 +0100

    implement such function to select groups.

--- cast-to-client.js
+++ cast-to-client.js
@@ -813,6 +813,37 @@ module.exports = function (RED) {
             // this.status({fill:"red",shape:"dot",text:"error - input parameter"});
             return null;
         });
+
+        discoverIpAddresses('googlecast', function (ipaddresses) {
+            RED.httpAdmin.get('/ipaddresses', function (req, res) {
+                res.json(ipaddresses);
+            });
+        });
     }
+
+    function discoverIpAddresses(serviceType, discoveryCallback) {
+        var ipaddresses = [];
+        var bonjour = require('bonjour')();
+        var browser = bonjour.find({
+          type: serviceType
+        }, function (service) {
+          service.addresses.forEach(function (ip) {
+            if (ip.split(".").length == 4) {
+              ipaddresses.push({
+                ip: ip,
+                port: service.port,
+                label: service.txt.md            
+              });
+            }
+          });
+    
+          // delay for all services to be discovered
+          if (discoveryCallback)
+            setTimeout(function () {
+              discoveryCallback(ipaddresses);
+            }, 2000);
+        });
+     }
+
     RED.nodes.registerType('cast-to-client', CastNode);
 };
